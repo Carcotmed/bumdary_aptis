ccad1a17c7fe04e60cd4b908bf1f71a22d315af4
Carlos Cote Medina <carcotmed@gmail.com>
2021-01-25 09:59:10 +0100

    Removed everything notes-related

--- index.js
+++ index.js
@@ -12,13 +12,11 @@ app.get("/", function (req, res) {
     res.send("hello world");
 });
 
-const notesApi = require("./routes/notes.js");
 const aptisApi = require("./routes/aptis.js");
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-notesApi(app);
 aptisApi(app);
 
 app.listen(config.port, function () {
